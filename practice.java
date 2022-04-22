36a078cf56632cab8e21051e683ebc0ece54525f
surya <suryanarayana4870@gmail.com>
2022-04-22 15:22:31 +0530

    first commit

---
+++ practice.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class practice{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter your word :");
+    String word = scan.nextLine();
+    String result = "";
+
+    for(int i = 0; i < word.length(); i++){
+      if(word.charAt(i) == 'e'){
+        result+='i';
+      }else{
+        result+=word.charAt(i);
+      }
+    }
+    System.out.println(result);
+   }
+}
